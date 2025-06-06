b843ddcc52700e01c29f4a79627affc96f53e0d1
hjw9623 <hjw6191.wh@gmail.com>
2025-06-07 07:34:12 +0900

    refactor: API주소 변경

--- app/src/main/java/com/example/capstonedesign/utils/Constants.kt
+++ app/src/main/java/com/example/capstonedesign/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.capstonedesign.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://192.168.37.200:8081"
+        const val BASE_URL = "http://10.0.2.2:8081"
         const val TAG = "CameraX"
         const val KAKAO_API_KEY = "KakaoAK e04b32764dc12bad73a88278d6df550b"
     }
